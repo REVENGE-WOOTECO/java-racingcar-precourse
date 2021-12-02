52ea9946fead1212ef24c3e5083fbf28e1d512e4
leejohy-0223 <leejohy@naver.com>
2021-12-02 20:46:43 +0900

    feat(CarController): 검증된 차 이름 리스트를 기반으로 CarController 객체 생성

--- src/main/java/racingcar/domain/CarController.java
+++ src/main/java/racingcar/domain/CarController.java
@@ -1,10 +1,18 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CarController {
-    public void makeCarList(List<String> cars) {
 
+    private static final List<Car> carList = new ArrayList<>();
+
+    public static CarController makeCarList(List<String> cars) {
+
+        for (String carName : cars) {
+            carList.add(new Car(carName));
+        }
+        return new CarController();
     }
 
     public List<String> gameStart(int repeatCount) {
