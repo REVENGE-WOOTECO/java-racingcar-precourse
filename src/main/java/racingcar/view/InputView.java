da6604b2eb97705f556157514c221d7c1be15be9
leejohy-0223 <leejohy@naver.com>
2021-12-02 22:23:07 +0900

    debug: console.readLine으로 사용자 입력 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,11 +3,10 @@ package racingcar.view;
 import java.util.List;
 import java.util.Scanner;
 
+import camp.nextstep.edu.missionutils.Console;
 import racingcar.utils.InputValidator;
 
 public class InputView {
-
-    private static final Scanner sc = new Scanner(System.in);
 
     public static List<String> requestCarInput() {
         List<String> result;
@@ -15,7 +14,7 @@ public class InputView {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 
         do {
-            input = sc.nextLine();
+            input = Console.readLine();
         } while ((result = InputValidator.validateNameList(input)) == null);
 
         return result;
@@ -26,7 +25,7 @@ public class InputView {
         System.out.println("시도할 회수는 몇회인가요?");
 
         do {
-            input = sc.nextLine();
+            input = Console.readLine();
         } while (!InputValidator.validateCount(input));
 
         return Integer.parseInt(input);
