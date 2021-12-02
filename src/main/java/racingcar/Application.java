8a75b93c9b0042683f635e1c8d37f8a4bbeca863
leejohy-0223 <leejohy@naver.com>
2021-12-02 20:47:36 +0900

    refactor(Application): static factory method를 사용하도록 변경

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,10 +8,8 @@ import racingcar.view.InputView;
 public class Application {
     public static void main(String[] args) {
 
-        CarController carController = new CarController();
-
         List<String> cars = InputView.requestCarInput();
-        carController.makeCarList(cars);
+        CarController carController = CarController.makeCarList(cars);
 
         int repeatCount = InputView.requestRepeatCount();
         carController.gameStart(repeatCount);
