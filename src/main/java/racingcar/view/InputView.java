8bd7c713c12ce2ac6569552fd8cd3e6655d934ea
yuminhwan <ymh7532@icloud.com>
2021-12-03 16:07:14 +0900

    feat: 잘못된 자동차 이름 입력일 경우 에러 발생, 메시지 출력 후 다시 입력받는 기능 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -11,11 +11,32 @@ public class InputView {
     private final static String REQUEST_INPUT_TRY_NUMBER = "시도할 회수는 몇회인가요?";
     private final static String COMMA_DELIMITER = ",";
 
+    private final static String ERROR_WHITE_SPACE_NAME = "[ERROR] 공백인 이름은 입력할 수 없습니다. 다시 입력해주세요.";
+    private final static String ERROR_NAME_LENGTH = "[ERROR] 길이가 5 초과인 이름은 입력할 수 없습니다. 다시 입력해주세요.";
+    private final static String ERROR_DUPLICATE_NAME = "[ERROR] 동일한 이름은 입력할 수 없습니다. 다시 입력해주세요.";
+
     public List<String> inputCarNames() {
         System.out.println(REQUEST_INPUT_CAR_NAMES);
-        String inputCarNames = Console.readLine();
-        return Arrays.stream(inputCarNames.split(COMMA_DELIMITER))
+        List<String> carNames = splitAndTrimInputCarNames();
+        while (!isValidInputCarNames(carNames)) {
+            carNames = splitAndTrimInputCarNames();
+        }
+        return carNames;
+    }
+
+    private List<String> splitAndTrimInputCarNames() {
+        String[] inputCarNames = Console.readLine().split(COMMA_DELIMITER, -1);
+        return Arrays.stream(inputCarNames)
             .map(String::trim)
             .collect(Collectors.toList());
+    }
+
+    private boolean isValidInputCarNames(List<String> carNames) {
+        try {
+            return true;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+        return false;
     }
 }
