7bba036646d1ec5a01089daf96df24ccd18f8d67
moduhan <devduhanmo@gmail.com>
2021-12-06 16:27:11 +0900

    feat: 사용자가 이동할 값을 입력받는 기능 구현

--- src/main/java/racingcar/util/UserInput.java
+++ src/main/java/racingcar/util/UserInput.java
@@ -2,15 +2,21 @@ package racingcar.util;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.Arrays;
+import static camp.nextstep.edu.missionutils.Console.*;
+import static java.lang.Integer.*;
+import static java.util.Arrays.stream;
 
 public class UserInput {
 
     private static final String COMMA = ",";
 
     public String[] carsInput() {
-        return Arrays.stream(Console.readLine().split(COMMA))
+        return stream(readLine().split(COMMA))
                 .map(String::trim)
                 .toArray(String[]::new);
+    }
+
+    public int stepInput() {
+        return parseInt(readLine());
     }
 }
