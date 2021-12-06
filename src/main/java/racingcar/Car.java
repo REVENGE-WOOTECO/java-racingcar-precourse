52ffb0f5431ba2cb2beee7e0ca46c7058d7db433
moduhan <devduhanmo@gmail.com>
2021-12-06 22:33:31 +0900

    feat: 자동차 클래스를 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,36 @@
 package racingcar;
 
-public class Car {
+public class Car implements Comparable<Car>{
+
+    private final StringBuilder positionGraph;
     private final String name;
     private int position = 0;
 
     public Car(String name) {
         this.name = name;
+        this.positionGraph = new StringBuilder();
     }
 
-    // 추가 기능 구현
+    public void forward() {
+        position++;
+        positionGraph.append("-");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    @Override
+    public String toString() {
+        return name + " : " + positionGraph;
+    }
+
+    @Override
+    public int compareTo(Car other) {
+        return other.position - this.position;
+    }
 }
