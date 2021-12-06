fc2d1658512ba8bc3c81ceea4b7e3388d6a017fc
moduhan <devduhanmo@gmail.com>
2021-12-07 00:04:17 +0900

    style: 게임실행시 게임안내문 변경

--- src/main/java/racingcar/game/Game.java
+++ src/main/java/racingcar/game/Game.java
@@ -34,12 +34,12 @@ public class Game {
     }
 
     private String[] inputCarNames() {
-        System.out.println("경주할 자동차 이름");
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return userInput.inputCars();
     }
 
     private int inputTryCount() {
-        System.out.println("시도할 횟수");
+        System.out.println("시도할 회수는 몇회인가요?");
         return userInput.inputTryCount();
     }
 
@@ -52,6 +52,7 @@ public class Game {
     }
 
     private void executeAsTryCount(GameInfo gameInfo) {
+        System.out.println("\n실행 결과");
         for (int i = 0; i < gameInfo.getTryCount(); i++) {
             moveCars(gameInfo);
             printCarsPosition(gameInfo);
