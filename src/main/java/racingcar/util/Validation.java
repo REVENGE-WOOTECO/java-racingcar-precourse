9f0b2134c82d8cd8ff259ae13c6a563a7ee37c96
moduhan <devduhanmo@gmail.com>
2021-12-06 19:33:33 +0900

    feat: 사용자의 입력 예외 처리 구현(입력횟수가 숫자가 아닐 시)

--- src/main/java/racingcar/util/Validation.java
+++ src/main/java/racingcar/util/Validation.java
@@ -1,6 +1,7 @@
 package racingcar.util;
 
 import java.util.Set;
+import java.util.regex.Pattern;
 
 import static java.util.Arrays.stream;
 import static java.util.stream.Collectors.toSet;
@@ -8,16 +9,24 @@ import static java.util.stream.Collectors.toSet;
 public class Validation {
 
     private static final int NAME_MAX_LENGTH = 5;
+    private static final String PATTERN_REGEX = "-?\\d+";
+    private static final Pattern PATTERN = Pattern.compile(PATTERN_REGEX);
 
     public void validateCarNames(String[] carNames) {
         validateMaxLength(carNames);
         validateDuplicateCarName(carNames);
     }
 
+    public void validateIsNumber(String numberString) {
+        if (!PATTERN.matcher(numberString).matches()) {
+            throw new IllegalArgumentException("[ERROR] 시도 횟수는 숫자여야 합니다.");
+        }
+    }
+
     private void validateMaxLength(String[] carNames) {
         for (String carName : carNames) {
             if (carName.length() > NAME_MAX_LENGTH) {
-                throw new IllegalArgumentException("자동차명의 최대길이는 5입니다");
+                throw new IllegalArgumentException("[ERROR] 자동차명의 최대길이는 5입니다.");
             }
         }
     }
@@ -25,7 +34,7 @@ public class Validation {
     private void validateDuplicateCarName(String[] carNames) {
         Set<String> carNameSet = stream(carNames).collect(toSet());
         if (carNameSet.size() != carNames.length) {
-            throw new IllegalArgumentException("중복되는 자동차명이 존재합니다");
+            throw new IllegalArgumentException("[ERROR] 중복되는 자동차명이 존재합니다.");
         }
     }
 }
