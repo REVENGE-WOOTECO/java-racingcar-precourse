cc1deb335439a005e7fbc034d5782c196e3cd791
moduhan <devduhanmo@gmail.com>
2021-12-07 00:20:27 +0900

    refactor: 에러메시지 [ERROR] PREFIX 상수화

--- src/main/java/racingcar/util/Validator.java
+++ src/main/java/racingcar/util/Validator.java
@@ -10,6 +10,7 @@ public class Validator {
 
     private static final int NAME_MAX_LENGTH = 5;
     private static final String PATTERN_REGEX = "-?\\d+";
+    private static final String PREFIX = "[ERROR]";
     private static final Pattern PATTERN = Pattern.compile(PATTERN_REGEX);
 
     public void validateCarNames(String[] carNames) {
@@ -19,14 +20,14 @@ public class Validator {
 
     public void validateIsNumber(String numberString) {
         if (!PATTERN.matcher(numberString).matches()) {
-            throw new IllegalArgumentException("[ERROR] 시도 횟수는 숫자여야 합니다.");
+            throw new IllegalArgumentException(PREFIX + "시도 횟수는 숫자여야 합니다.");
         }
     }
 
     private void validateMaxLength(String[] carNames) {
         for (String carName : carNames) {
             if (carName.length() > NAME_MAX_LENGTH) {
-                throw new IllegalArgumentException("[ERROR] 자동차명의 최대길이는 5입니다.");
+                throw new IllegalArgumentException(PREFIX + "자동차명의 최대길이는 5입니다.");
             }
         }
     }
@@ -34,7 +35,7 @@ public class Validator {
     private void validateDuplicateCarName(String[] carNames) {
         Set<String> carNameSet = stream(carNames).collect(toSet());
         if (carNameSet.size() != carNames.length) {
-            throw new IllegalArgumentException("[ERROR] 중복되는 자동차명이 존재합니다.");
+            throw new IllegalArgumentException(PREFIX + "중복되는 자동차명이 존재합니다.");
         }
     }
 }
