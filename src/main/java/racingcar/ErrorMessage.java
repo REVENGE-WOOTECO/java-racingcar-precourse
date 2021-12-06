71fde17616c02e4b8a2c56db9dd13f21711f07c3
highright96 <nsw5296@naver.com>
2021-12-06 23:51:26 +0900

    feat: 에러 메시지 클래스 추가

---
+++ src/main/java/racingcar/ErrorMessage.java
@@ -0,0 +1,9 @@
+package racingcar;
+
+public class ErrorMessage {
+
+    public static final String DUPLICATED_CAR_NAME = "[ERROR] 중복된 자동차 이름을 입력했습니다.";
+    public static final String INVALID_CAR_NAME_LENGTH = "[ERROR] 입력한 자동차 이름이 허용된 길이를 넘었습니다.";
+    public static final String INVALID_TOTAL_ROUND = "[ERROR] 잘못된 이동 횟수를 입력했습니다.";
+    public static final String INVALID_TOTAL_ROUND_RANGE = "[ERROR] 잘못된 범위의 이동 횟수를 입력했습니다.";
+}
