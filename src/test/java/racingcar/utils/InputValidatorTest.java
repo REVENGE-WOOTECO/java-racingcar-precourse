d671c94edfa7f47f8cdc024927c0790dc812f68a
leejohy-0223 <leejohy@naver.com>
2021-12-07 10:54:08 +0900

    refactor(InputValidatorTest): @DisplayName 적용

--- src/test/java/racingcar/utils/InputValidatorTest.java
+++ src/test/java/racingcar/utils/InputValidatorTest.java
@@ -11,39 +11,44 @@ import org.junit.jupiter.params.provider.ValueSource;
 class InputValidatorTest {
 
     @ParameterizedTest
+    @DisplayName("알파벳이 아닌 값이 들어온 경우 빈 List를 반환한다.")
     @ValueSource(strings = {"123", "345", "가나다", "abc3", "4fjd", "ab, cd", "Ab cd"})
-    void regex_알파벳_아닌_값_검증(String input) {
-        assertThat(InputValidator.validateNameList(input).isEmpty()).isEqualTo(true);
+    void regex_no_alphabet(String input) {
+        assertThat(InputValidator.validateNameList(input).isEmpty()).isTrue();
     }
 
     @ParameterizedTest
+    @DisplayName("길이가 1 ~ 5가 아닐 경우 빈 List를 반환한다.")
     @ValueSource(strings = {"a,bcdefg,d", ",bcd,def", "afdfdf,asdfasa,bbbbbbb"})
-    void length_실패_검증(String input) {
-        assertThat(InputValidator.validateNameList(input).isEmpty()).isEqualTo(true);
+    void length_fail(String input) {
+        assertThat(InputValidator.validateNameList(input).isEmpty()).isTrue();
     }
 
     @ParameterizedTest
+    @DisplayName("정상적인 문자열이 입력된 경우 해당 사이즈를 반환한다.")
     @CsvSource({"'abc', 1", "'dfefs', 1", "'lee,jh', 2", "'jo,hy,pobi', 3", "'dfs,hy', 2"})
-    void regex_정상_입력_검증(String input, int result) {
+    void regex_normal(String input, int result) {
         assertThat(InputValidator.validateNameList(input).size()).isEqualTo(result);
     }
 
     @ParameterizedTest
+    @DisplayName("0 이상의 숫자가 아닐 경우 false 를 반환한다.")
     @ValueSource(strings = {"a", "bcd", "1db", "-12", "2af", "-234"})
-    void count_실패_검증(String input) {
-        assertThat(InputValidator.validateCount(input)).isEqualTo(false);
+    void count_fail(String input) {
+        assertThat(InputValidator.validateCount(input)).isFalse();
     }
 
     @ParameterizedTest
+    @DisplayName("0이상의 숫자일 경우 True 를 반환한다.")
     @ValueSource(strings = {"12", "1", "0", "23", "199", "77777"})
-    void count_성공_검증(String input) {
-        assertThat(InputValidator.validateCount(input)).isEqualTo(true);
+    void count_success(String input) {
+        assertThat(InputValidator.validateCount(input)).isTrue();
     }
 
     @ParameterizedTest
+    @DisplayName("중복된 이름이 있을 경우 빈 List를 반환한다.")
     @ValueSource(strings = {"lee,lee", "jo,jo", "aaa,aaa"})
-    void 중복_이름_실패_검증(String input) {
-        assertThat(InputValidator.validateNameList(input).isEmpty()).isEqualTo(true);
+    void validate_duplicated_name(String input) {
+        assertThat(InputValidator.validateNameList(input).isEmpty()).isTrue();
     }
-
 }
