cbedffab9f7e74524fb81a30c94c33a8da65343e
yuminhwan <ymh7532@icloud.com>
2021-12-12 03:50:26 +0900

    refactor: 상수 사용

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -5,6 +5,7 @@ import racingcar.strategy.MovableStrategy;
 public class Car {
     private static final String ERROR_WHITE_SPACE_NAME = "[ERROR] 공백인 이름은 입력할 수 없습니다. 다시 입력해주세요.";
     private static final String ERROR_NAME_LENGTH = "[ERROR] 길이가 5 초과인 이름은 입력할 수 없습니다. 다시 입력해주세요.";
+    private static final int MAX_NAME_LENGTH = 5;
 
     private final String name;
     private final MovableStrategy movableStrategy;
@@ -24,7 +25,7 @@ public class Car {
     }
 
     private void validateNameLength(String name) {
-        if (name.length() > 5) {
+        if (name.length() > MAX_NAME_LENGTH) {
             throw new IllegalArgumentException(ERROR_NAME_LENGTH);
         }
     }
